05a8faa7f0ae090b6d72b581929e99ca3402ee66
Cerisezmx <ceriseZmx@163.com>
2018-10-13 23:56:20 +0800

    主页部分还剩footer部分 10/13

--- js/scroll.js
+++ js/scroll.js
@@ -3,6 +3,15 @@ define(['jquery'],function($){
 		var showIndex = 0; //用于记录下标
 		var timer; //定时器
 		$(function() {
+			//wechat动画部分
+			$('#wechat').on('mouseenter','#wxMall,#weichatCon',function(){
+				$("#weichatCon").stop(true).animate({height:180,},1000).css({display:'block'});
+				return false;
+			})
+			$('#wechat').on('mouseleave','#wxMall,#weichatCon',function(){
+				$('#weichatCon').stop(true).animate({height:0},100).css({display: 'none'});
+				return false;
+			})
 			//轮播图动画
 			$("ul.uItems li").not(":eq(0)").css("display", "none");
 			startTimer();
@@ -34,14 +43,13 @@ define(['jquery'],function($){
 					//轮播图图片
 					var scrollM = data.scrollImg;
 					for(var i = 0; i< scrollM.length; i++){
-						$(`<li><img src="${scrollM[i].src}"/></li>`).appendTo('.uItems');
+						$(`<li><img src="${scrollM[i].src}"/></li>`).appendTo($('.uItems'));
 					}
-					
 					//轮播图左侧导航栏
 					var leftdata = data.scroll;
 					for(var i = 0 ; i < leftdata.length;i++){
 						$(`<li>${leftdata[i].listname}<span>></span></li>
-						    `).appendTo('#tabOl');
+						    `).appendTo($('#tabOl'));
 					}
 					for(var k = 0 ; k < leftdata.length ; k++){
 						$(`<ul class="slConUl"}></ul>`).appendTo('.leftList');
@@ -58,7 +66,7 @@ define(['jquery'],function($){
 					//轮播图左边动画部分
 					var oLis = $('#tabOl').find('li');
 					var oUls = $('.leftList').find('ul');
-					$('.leftList').on('mouseenter','li',function(){
+					$('.leftList').on('mouseenter','#tabOl>li',function(){
 						oUls.css('display','none').eq($(this).index()).css('display','block');
 						oLis.removeClass('scLeftactive').eq($(this).index()).addClass('scLeftactive');
 					})
@@ -71,7 +79,7 @@ define(['jquery'],function($){
 					//导航头
 					for(var i = 0 ; i < topdata.length;i++){
 						$(`<li><a href="#">${topdata[i].topnav}</a></li>`)
-						.appendTo('.navOl');
+						.appendTo($('.navOl'));
 					}
 					//导航内容
 					for(var k = 0 ; k < topdata.length ; k++){
@@ -88,20 +96,80 @@ define(['jquery'],function($){
 					//头部导航内容显示的动画
 					var topLis = $('.navOl').find('li');
 					var topDivs = $('.topnav').find('div');
-					$('.topnav').on('mouseenter','li',function(){
+					$('.topnav').on('mouseenter','.navOl>li',function(){
 						topDivs.css('display','none').eq($(this).index()).css('display','block');
 						topLis.find('a').removeClass('active').eq($(this).index()).addClass('active');
 					})
 					$('.topnav').on('mouseleave',function(){
 						topDivs.css('display','none');
-						topLis.removeClass('active')
+						topLis.find('a').removeClass('active');
 					})
+					//midAdv中间的广告图片加载
+					var adv = data.midAdv;
+					for(var i = 0 ; i < adv.length;i++){
+						$(`<a href=""><img src="${adv[i].src}"
+						 ></a>`).appendTo($('.midACon'));
+					}
+					//主要商品部分(导航栏title)：
+					var advConTitle = data.mainTitle;
+					//循环加div 和title
+					for(var i = 0 ; i < advConTitle.length;i++){
+						$(`<div class='MainCon'><dl class='MainConTitle'>
+							<dt class='MainConTitleBig'><a href="">${advConTitle[i].titleBig}</a></dt>
+							<a href="#" id = 'viewMore'>更多 ></a>
+							</dl><div class= 'MainAd'></div></div>`).appendTo($('#main'));
+						var advConTitleS = advConTitle[i].titleSmall;
+						for(var k = 0 ; k < advConTitleS.length; k++){
+							$(`<dt class='MainConTitleSmall'><a href="">${advConTitleS[k]}</a></dt>`).appendTo($('.MainConTitle')[i]);
+						}
+					}
+					//加载每个商品
+					var mainCon = data.mainContent;
+					//外层循环，添加ul
+					for(var i = 0 ; i < mainCon.length; i++){
+						$(`<a class='MainAdLeft'>
+						<img src="${mainCon[i].leftImg}" alt="dsdd">
+						</a><ul class='MainAdUl'></ul>`).appendTo($('.MainAd')[i]);
+						//内层循环，加li
+						var mainConLi = mainCon[i].rightUl;
+						for(var j  = 0 ; j < mainConLi.length ; j++){
+							$(`<li class='limove'>
+								<a href="#">
+									<img src="${mainConLi[j].src}" alt="">
+									<p>${mainConLi[j].title}</p>
+									<i>${mainConLi[j].dice}</i>
+									<strong>${mainConLi[j].price}</strong>
+								</a>
+							</li>`).appendTo($('.MainAdUl')[i]);
+						}
+					}
+					//商品详情区域li的动画
+					$('#main').on('mouseenter','.limove',function(){
+						$(this).stop().animate({top:-7},200).css('boxShadow','0 0 18px #ccc');
+					});
+					$('#main').on('mouseleave','.limove',function(){
+						$(this).stop(true).animate({top:0},100).css('boxShadow','');
+					})
+					//News添加数据
+					var Newsdata = data.News;
+					for(var i = 0 ; i < Newsdata.length ; i++){
+						$(`<li class='newsLi'>
+							<a href="#">
+							<img src="${Newsdata[i].src}" alt="news1">
+							</a>
+						<span>新闻</span>
+						<p>${Newsdata[i].title}</p>
+						<i>${Newsdata[i].content}</i>
+						<a href="#" class='NewsA'>查看详情  >></a>
+					</li>`).appendTo('.newsUl');
+					}
 				},
 				error:function(msg){
 					console.log(msg);
 				}
 			})
 		});
+		//轮播图的动画
 		function startTimer(){
 			timer = setInterval(function() {
 				showIndex++;
